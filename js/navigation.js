80e74eb3bbedfd78d414e9cb6224d4113b72dad2
Simon Philpott <sideburnt@gmail.com>
2019-12-18 11:27:44 +0000

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -59,7 +59,9 @@ const matchPageToMenu = () => {
 document.addEventListener("DOMContentLoaded", function() {
     if (location.hash){
         let targetMenuItem = location.hash.substr(1);
-        console.log(document.querySelector([data-identity=targetMenuItem]));
+        //console.log(document.querySelector([data-identity=targetMenuItem]));
+        console.log(targetMenuItem);
+        console.log(`[data-identity='${targetMenuItem}']`);
     }    
     if (window.matchMedia('(min-width: 1024px)').matches) {
         //Wide viewport
